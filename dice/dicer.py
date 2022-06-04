8ac0f70b871492ff66c3f5f92264833811dd81fb
Markku Sukanen <markku.sukanen@gmail.com>
2022-06-04 09:18:11 +0300

    fiddlery

--- dice/dicer.py
+++ dice/dicer.py
@@ -1,10 +1,10 @@
 import random
 
-def __anyD(num, x) -> int:
-    """Roll `num` `x`-sided dice.
+def __anyD(num:int, x:int) -> int:
+    """Roll `num` of `x`-sided dice.
     
-    :param
-    :returns: `int`"""
+    :param `num`: number of dice.
+    :param `x`: sides per die."""
     if (num == 0) or (x == 0):
         return 0
     if (not isinstance(num, int)) or (not isinstance(x, int)):
@@ -14,16 +14,14 @@ def __anyD(num, x) -> int:
         r += random.randint(1, abs(x))
     return r
 
-def d6(num) -> int:
+def d6(num:int = 1) -> int:
     """Roll `num` D6's.
     
-    :param num int: `num` of rolls.
-    :returns: `int`"""
+    :param `num`: number of dice."""
     return __anyD(num, 6)
 
-def d10(num) -> int:
+def d10(num:int = 1) -> int:
     """Roll `num` D10's.
     
-    :param: `num` of rolls.
-    :returns: `int`"""
+    :param `num`: number of dice."""
     return __anyD(num, 10)
